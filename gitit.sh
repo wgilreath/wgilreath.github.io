e59ea30d0530d67cdda65dfcfd5856822c1f4130
William Gilreath <williamgilreath@Pugsley-Macbook.local>
2017-07-27 18:09:51 -0700

    Test

--- gitit.sh
+++ gitit.sh
@@ -8,12 +8,17 @@ then
 fi
 
 #create html index
+echo "\nCreating index of directory for github.io\n"
+	
 java -jar MkIndex.jar
 mv index-byname.html html
 
+echo "\nUpdating github.io with files\n"
+	
 #update github.io with directory
 git add --all
 git commit -m $1
 git push -u origin master
 
-
+echo "\nAll done!\n"
+	
